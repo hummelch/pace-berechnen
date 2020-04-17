0b2cb784eba9cc8611ec030ffbb05ed8bd624c66
Christoph Hummel <christoph.hummel@badenova.de>
2020-04-17 18:47:09 +0200

    retry

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 export default {
     input: './src/main.js',
     output: {
-        file: './build/bundle.min.js',
+        file: './dist/bundle.min.js',
         format: 'iife',
         name: 'bundle'
     }
